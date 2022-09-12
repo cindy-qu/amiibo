487c34682820c9d7a8c0b1499add6b6eab539c40
cindy-qu <109881408+cindy-qu@users.noreply.github.com>
2022-09-12 15:34:55 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
-fetch('http://localhost:3000/amiibo')
-.then(res => res.json())
-.then(character => {
-    // character.forEach(amiibo =>{console.log(amiibo.name)})
 
+const amiiboSpan = document.querySelector('#namesList')
+
+
+
+amiiboSpan.addEventListener('click', e => {
     const amiiboName = document.querySelector('#name')
-    amiiboName.textContent = `Name: ${character[0].name}`
+    amiiboName.textContent = `Name: ${amiibo.name}`
 
 
 
     const amiiboGameSeries= document.querySelector('#gameseries')
-    amiiboGameSeries.textContent = `Game Series: ${character[0].gameSeries}`
+    amiiboGameSeries.textContent = `Game Series: ${amiibo.gameSeries}`
 
     const amiiboReleaseDate= document.querySelector('#releasedate')
-    amiiboReleaseDate.textContent= `N. America Release date: ${character[0].release.na}`
+    amiiboReleaseDate.textContent= `N. America Release date: ${amiibo.release.na}`
     
     const amiiboType= document.querySelector('#type')
-    amiiboType.textContent = `Type: ${character[0].type}`
+    amiiboType.textContent = `Type: ${amiibo.type}`
 
 
 
     const image = document.querySelector("#image")
-    image.src = character[0].image
+    image.src = amiibo.image
+})
+
+
+fetch('http://localhost:3000/amiibo')
+.then(res => res.json())
+.then(character => {
+    character.forEach(amiibo =>{
+
+    //Amiibo Names
+    const amiiboList = document.querySelector('#namesList')
+  const newList = document.createElement('span')
+  newList.textContent = amiibo.name
+
+  amiiboList.append(newList)
+
+
+     //Amiibo Details   
+    const amiiboName = document.querySelector('#name')
+    amiiboName.textContent = `Name: ${amiibo.name}`
+
+
+
+    const amiiboGameSeries= document.querySelector('#gameseries')
+    amiiboGameSeries.textContent = `Game Series: ${amiibo.gameSeries}`
+
+    const amiiboReleaseDate= document.querySelector('#releasedate')
+    amiiboReleaseDate.textContent= `N. America Release date: ${amiibo.release.na}`
     
-    const amiibo = character.name
+    const amiiboType= document.querySelector('#type')
+    amiiboType.textContent = `Type: ${amiibo.type}`
+
+
+
+    const image = document.querySelector("#image")
+    image.src = amiibo.image
+    
+    
 
     
-
+})
 
 })
