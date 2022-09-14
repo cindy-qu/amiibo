f8c746b6ff804b9a9bcdf4440bbee2bdbea4fd31
Cindy Qu <cindyqu42@gmail.com>
2022-09-14 08:07:34 -0700

    updated css

--- index.js
+++ index.js
@@ -7,87 +7,76 @@ const amiiboReleaseDate= document.querySelector('#releasedate')
 const amiiboType= document.querySelector('#type')
 const image = document.querySelector("#image")
 
-
-// const wishlist = document.querySelector('#wishlist')
-//     wishlist.addEventListener('click', e => {
-   
-//         wishlist.textContent = "♥"
-
-//         e.target.style.color = 'yellow';
-        
-//     })
+//Thumsup and Thumbsdown Button
 const btn1 = document.querySelector('#green');
 const btn2 = document.querySelector('#red');
 
 btn1.addEventListener('click', function() {
-  
     if (btn2.classList.contains('red')) {
       btn2.classList.remove('red');
     } 
   this.classList.toggle('green');
-  
 });
 
 btn2.addEventListener('click', function() {
-  
     if (btn1.classList.contains('green')) {
       btn1.classList.remove('green');
     } 
   this.classList.toggle('red');
-  
 });
 
   
 
-
+//Fetch request
 fetch('http://localhost:3000/amiibo')
 .then(res => res.json())
 .then(character => {
     renderAmiibo(character);
-    console.log(character[0])
-
+    renderMario(character[0])
 })
 
+//Function to set default image to the first character on the database list
+function renderMario(character){
+    amiiboName.textContent = `${character.name}`;
+    image.src = character.image
+    image.addEventListener('click', e => {
+        amiiboGameSeries.textContent = `Game Series: ${character.gameSeries}`
+        amiiboReleaseDate.textContent= `North America Release date: ${character.release.na}`
+        amiiboType.textContent = `Type: ${character.type}`
+    })
+}
+
+
+
+//Function to render Amiibo details for each name
 function renderAmiibo(character){
     character.forEach(amiibo =>{
 
     //Amiibo Names
     const amiiboList = document.querySelector('#namesList')
-    
     const newList = document.createElement('span')
-    
+    newList.id = 'hovercolor'
     newList.textContent = amiibo.name
 
     amiiboList.append(newList)
 
 
-    
+    //Click on name to render Amiibo name and image
     newList.addEventListener('click', e => {
     
-        amiiboName.textContent = `Name: ${amiibo.name}`
-    
-        // amiiboGameSeries.textContent = `Game Series: ${amiibo.gameSeries}`
-    
-        // amiiboReleaseDate.textContent= `N. America Release date: ${amiibo.release.na}`
-        
-        // amiiboType.textContent = `Type: ${amiibo.type}`
-    
+        amiiboName.textContent = `${amiibo.name}`
         image.src = amiibo.image
 
+        //Click on image to show Amiibo series, release date, and type
         image.addEventListener('click', e => {
-            // amiiboName.textContent = `Name: ${amiibo.name}`
-            
             amiiboGameSeries.textContent = `Game Series: ${amiibo.gameSeries}`
-        
-            amiiboReleaseDate.textContent= `N. America Release date: ${amiibo.release.na}`
-            
+            amiiboReleaseDate.textContent= `North America Release date: ${amiibo.release.na}`
             amiiboType.textContent = `Type: ${amiibo.type}`
-
-           
-           
         })
      
     })
+
+    //Event listener to hide the Amiibo details when you click on a different name
     newList.addEventListener('click', e => {
         amiiboGameSeries.textContent = ""
 
